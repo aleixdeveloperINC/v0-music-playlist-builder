9c4cd6de63748e8cba5737abc6224631c8a40f5e
Aleix <aleixclemente@gmail.com>
2026-01-24 16:01:19 +0100

    feat: add ESLint configuration file

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import tseslint from "typescript-eslint";
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+
+export default tseslint.config(
+  {
+    ignores: [".next/**"], // Ignore the .next directory
+  },
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    plugins: {
+      "@next/next": nextPlugin,
+      react: reactPlugin,
+      "react-hooks": hooksPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...hooksPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+);
